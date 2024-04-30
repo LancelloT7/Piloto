3a2ac11a6311ce90b69f9c8d8bb0ded9bd0e591c
LancelloT7 <matias7g@hotmail.com>
2024-04-30 12:11:16 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+
+
+class Program
+{   
+     static void Main(string[] args)
+    {
+       Cadastrar();
+    }  
+
+    public class Produto
+    {
+      public string Nome {get; set;} 
+      public string Idade {get; set;} 
+
+      public Produto(string nome,string idade) 
+      {
+         Nome = nome;
+         Idade = idade;
+      }
+    }
+
+    public static void Cadastrar ()
+    {
+      Console.WriteLine("Escreva um nome");
+      string nome = Console.ReadLine ();
+      Console.WriteLine("idade?");
+      string idade = Console.ReadLine ();
+
+      Produto produto = new Produto(nome, idade);
+
+      Console.WriteLine(produto.Nome + produto.Idade);
+    }
+
+
+    
+    }
+    
